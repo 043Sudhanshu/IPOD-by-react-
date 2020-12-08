cf02471cb05749367eab0e8f0089ceaa5cf2414d
sudhanshu chauhan <043sudhanshu@gmail.com>
2020-12-08 22:22:35 +0530

    Updates

---
+++ precache-manifest.24e60fc007ad18f08004083fdcb8cb78.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84ea005ee2388aac77c4ecbab2dc49d5",
+    "url": "/IPOD-by-react-/index.html"
+  },
+  {
+    "revision": "d35cb2a4df0c0730f0cc",
+    "url": "/IPOD-by-react-/static/css/main.fd69f81e.chunk.css"
+  },
+  {
+    "revision": "c2699f3d7716d5cf5499",
+    "url": "/IPOD-by-react-/static/js/2.603a3d61.chunk.js"
+  },
+  {
+    "revision": "3b6069e01f0db714f9e787b4dbe9775c",
+    "url": "/IPOD-by-react-/static/js/2.603a3d61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d35cb2a4df0c0730f0cc",
+    "url": "/IPOD-by-react-/static/js/main.69f613ec.chunk.js"
+  },
+  {
+    "revision": "58e5f6c3407bbf766734",
+    "url": "/IPOD-by-react-/static/js/runtime-main.8df3ee3b.js"
+  },
+  {
+    "revision": "641e6348f1866fbb4abd7da274dc2302",
+    "url": "/IPOD-by-react-/static/media/sickick.641e6348.mp3"
+  }
+]);
